6db0686fdfc043d0f8cdc35152943ad567979910
Steve-Kim <Stevekim0315@gmail.com>
2023-03-09 01:39:18 +1100

    11th commit

--- index.js
+++ index.js
@@ -65,24 +65,47 @@ app.get("/loadBusinessDetails", async (req, res) => {
 })
 
 
-app.post("/newInvoice",  (req, res) => {
+app.post("/newInvoice", async (req, res) => {
 
-    const { name, email } = req.body;
+    const { billtoEmail, billtoName, billtoFullAddress,billtoContactNumber,billtoItems,billingTotals,issueDate,dueDate,currentStatus } = req.body;
     
-    const obj = [{userName:name}]
-    // const getUserURL = await googleSheets.spreadsheets.values.get({
-    //     auth,
-    //     spreadsheetId,
-    //     range: "Users!B2"
-    // })
-    
-    // const spreadsheetIdForUser = await getUserURL.data.values[0][0]
+    // const obj = [{userName:name}]
 
-    // const businessDetalsSheetKeys = await googleSheets.spreadsheets.values.get({
-    //     auth,
-    //     spreadsheetId: spreadsheetIdForUser,
-    //     range: "BusinessDetails!A1:1"
-    // })
+    const getUserURL = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "Users!B2"
+    })
+    
+    const spreadsheetIdForUser = await getUserURL.data.values[0][0]
+
+
+    const getArrayOfInvoiceNums = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId: spreadsheetIdForUser,
+        majorDimension: "COLUMNS", // --> set values into an arry(all data in one array) from data and read
+        // majorDimension: "ROWS", // --> set values into arrays(one row one array) from data and read
+        range: "Invoices!A1:A", // --> from A2 to the lastRow
+        // range: "Bookings!A2:2", // --> from A2 to the lastColumn
+      })
+    
+    const prevInvoiceNum = await getArrayOfInvoiceNums.data.values[0][getArrayOfInvoiceNums.data.values[0].length - 1]
+
+    if (!isNaN(parseFloat(prevInvoiceNum)) && isFinite(prevInvoiceNum) && prevInvoiceNum >= 10000) {
+        var invoiceNum = await new Number(prevInvoiceNum) + 1
+        } else {
+        var invoiceNum = await new Number(10000)
+    }
+
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId: spreadsheetIdForUser,
+        range: "Invoices!A:Z",
+        valueInputOption: "USER_ENTERED",
+        resource:{
+          values:[[invoiceNum,billtoEmail, billtoName, billtoFullAddress,String("'"+billtoContactNumber),billtoItems,billingTotals,issueDate,dueDate,currentStatus]]
+        }
+    })
 
     // const businessDetailsData = await googleSheets.spreadsheets.values.get({
     //     auth,
@@ -105,7 +128,7 @@ app.post("/newInvoice",  (req, res) => {
     // console.log(businessDetails)
     // res.json([obj])
     // console.log(req.body)
-    res.json({invoiceNum: '9123999' })
+     res.json({invoiceNum })
 })
 
 
